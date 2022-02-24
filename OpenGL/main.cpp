137d4e34ceb0af992e787c5ab0bc8841024235e3
gokulkrh <gokulkrh@gmail.com>
2022-02-24 22:29:16 +0530

    more shader stuff

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -14,12 +14,12 @@ int main(int argc, char **argv) {
     SDL_Init(SDL_INIT_EVERYTHING);
 
     GLfloat vertices[] = {
-        -0.5, -0.5 * float(sqrt(3)) / 3, 0.0f,
-        0.5, -0.5 * float(sqrt(3)) / 3, 0.0f,
-        0.0, 0.5 * float(sqrt(3)) * 2 / 3, 0.0f,
-        -0.5/2, 0.5 * float(sqrt(3)) / 6, 0.0f,
-        0.5/2, 0.5 * float(sqrt(3)) / 6, 0.0f,
-        0.0, -0.5 * float(sqrt(3)) / 3, 0.0f,
+        -0.5, -0.5 * float(sqrt(3)) / 3, 0.0f,    0.8f, 0.3f, 0.02f,
+        0.5, -0.5 * float(sqrt(3)) / 3, 0.0f,     0.8f, 0.3f, 0.02f,
+        0.0, 0.5 * float(sqrt(3)) * 2 / 3, 0.0f,  1.0f, 0.6f, 0.32f,
+        -0.5/2, 0.5 * float(sqrt(3)) / 6, 0.0f,   0.9f, 0.45f, 0.17f,
+        0.5/2, 0.5 * float(sqrt(3)) / 6, 0.0f,    0.9f, 0.45f, 0.17f,
+        0.0, -0.5 * float(sqrt(3)) / 3, 0.0f,     0.8f, 0.3f, 0.02f,
     };
 
     GLuint indices[] = {
@@ -44,7 +44,8 @@ int main(int argc, char **argv) {
     VBO VBO1(vertices, sizeof(vertices));
     EBO EBO1(indices, sizeof(indices));
 
-    VAO1.LinkVBO(VBO1, 0);
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
