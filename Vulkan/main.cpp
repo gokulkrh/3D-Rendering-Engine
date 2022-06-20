87ce7dc359e6209b493a53685f60f6e5db91a957
gokulkrh <gokulkrh@gmail.com>
2022-06-20 13:41:04 +0530

    glfw window

--- Vulkan/main.cpp
+++ Vulkan/main.cpp
@@ -1,142 +1,48 @@
+#define GLFW_INCLUDE_NONE
 #include <vulkan/vulkan.h>
-#include <SDL2/SDL.h>
-#include <cstdlib>
-#include <stdexcept>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
-#include <vector>
-#include <optional>
+#include <stdexcept>
+#include <cstdlib>
 
-struct QueueFamilyIndices {
-    ::optional<uint32_t> graphicsFamily;
-
-    bool isComplete() {
-        return graphicsFamily.has_value();
-    }
-};
-
-class Application{
+class Application {
     public:
+        GLFWwindow* window;
+
         void run() {
-            InitWindow();
-            VulkInit();
-            Loop();
-            CleanUp();
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
         }
+
     private:
-        SDL_Window *window;
-        SDL_Event e;
-        VkInstance instance;
-        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+        void initWindow() {
+            if(!glfwInit())
+                std::cout << "failed to initialize" << "\n";
 
-        void InitWindow() {
-            SDL_Init(SDL_INIT_EVERYTHING);
-            window = SDL_CreateWindow("Hey look! A window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_VULKAN);
+            window = glfwCreateWindow(1280, 720, "Coolest window ever", nullptr, nullptr);
         }
 
-        void VulkInit() {
-            createInstance();
-            pickDevice();
+        void initVulkan() {
+            std::cout << "intializing vulkan" << "\n";
         }
 
-        void Loop() {
-            while(window){
-                SDL_UpdateWindowSurface(window);
-                if(SDL_PollEvent(&e)){
-                    if(e.type == SDL_QUIT || e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE){
-                        break;
-                    }
-                }
+        void mainLoop() {
+            while(!glfwWindowShouldClose(window)) {
+                glfwPollEvents();
             }
         }
 
-        void CleanUp() {
-            vkDestroyInstance(instance, nullptr);
-            SDL_DestroyWindow(window);
-            SDL_QUIT;
-        }
-
-        void createInstance() {
-            VkApplicationInfo appInfo{};
-            appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-            appInfo.pApplicationName = "Test App";
-            appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-            appInfo.pEngineName = "No Engine";
-            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-            appInfo.apiVersion = VK_API_VERSION_1_0;
-
-            VkInstanceCreateInfo createInfo{};
-            createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-            createInfo.pApplicationInfo = &appInfo;
-            createInfo.enabledLayerCount = 0;
-
-            if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-                throw std::runtime_error("failed to create instance!");
-            }
-        }
-
-        void pickDevice() {
-            uint32_t deviceCount = 0;
-            vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
-
-            if (deviceCount == 0) {
-                throw std::runtime_error("failed to find GPUs with Vulkan support!");
-            }
-            std::vector<VkPhysicalDevice> devices(deviceCount);
-            vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
-
-            for (const auto& device : devices) {
-                if (isDeviceSuitable(device)) {
-                    physicalDevice = device;
-                    break;
-                }
-            }
-
-            if (physicalDevice == VK_NULL_HANDLE) {
-                throw std::runtime_error("failed to find a suitable GPU!");
-            }
-        }
-        bool isDeviceSuitable(VkPhysicalDevice device) {
-            QueueFamilyIndices indices = findQueueFamilies(device);
-
-            return indices.isComplete();
-        }
-
-        QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
-            QueueFamilyIndices indices;
-
-            uint32_t queueFamilyCount = 0;
-            vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
-
-            std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
-            vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
-
-            int i = 0;
-            for (const auto& queueFamily : queueFamilies) {
-                if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-                    indices.graphicsFamily = i;
-                }
-
-                if (indices.isComplete()) {
-                    break;
-                }
-
-                i++;
-            }
-
-            return indices;
+        void cleanup() {
+            glfwDestroyWindow(window);
+            glfwTerminate();
         }
 };
 
-
-int main(int argc, char **argv){
-    Application app;
-
-    try{
-        app.run();
-    } catch (const std::exception& e){
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-
+int main() {
+    Application game;
+    game.run();
     return EXIT_SUCCESS;
 }
